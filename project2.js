21a0899feb6889a70dbe114a34f6087256294b2c
MohammedAlayed <alayedmohammed0@gmail.com>
2021-12-09 22:24:25 +0200

    create main,history,page,favourite with hide and show

---
+++ project2.js
@@ -0,0 +1,14 @@
+$(".showFavourite").on(`click`,()=>{
+    $(".Favourite").show()
+})
+$(".showpage").on(`click`,()=>{
+    $(".Favourite").hide()
+})
+$(".showHistory").on(`click`,()=>{
+    $(".history").show()
+})
+
+$(".showpage").on(`click`,()=>{
+    $(".history").hide()
+})
+
