77010c2d28101aca73765849fb52d9ef61777b29
MohammedAlayed <alayedmohammed0@gmail.com>
2021-12-10 02:06:46 +0200

    completed navbar

--- project2.js
+++ project2.js
@@ -1,14 +1,22 @@
+//show/hide Favourite
 $(".showFavourite").on(`click`,()=>{
     $(".Favourite").show()
 })
 $(".showpage").on(`click`,()=>{
     $(".Favourite").hide()
 })
+//show/hide history
 $(".showHistory").on(`click`,()=>{
     $(".history").show()
 })
-
 $(".showpage").on(`click`,()=>{
     $(".history").hide()
+})
+//show/hide top
+$(".showTop").on(`click`,()=>{
+    $(".top").show()
+})
+$(".showpage").on(`click`,()=>{
+    $(".top").hide()
 })
 
