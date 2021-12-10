6597a51f938922bbfda100d4f629da318ac6dbf3
MohammedAlayed <alayedmohammed0@gmail.com>
2021-12-10 20:49:11 +0200

    Top sort done

--- project2.js
+++ project2.js
@@ -229,3 +229,98 @@ TV.forEach(element=>{
     TV.appendTo(movieThree)
     })
 
+
+   //FAVourirte
+    let favourirte=[
+        {
+         link:'./img/ARMY.jpg',
+         description:"Lorem ipsum dolor sit amet", //p
+        },
+        {
+         link:'./img/ARMY.jpg',
+         description:"Lorem ipsum dolor sit amet ", 
+        },
+        {
+         link:'./img/ARMY.jpg',
+         description:"Lorem ipsum dolor sit amet ", 
+        },
+        {
+         link:'./img/ARMY.jpg',
+         description:"Lorem ipsum dolor sit amet  dsf", 
+        },
+        {
+         link:'./img/ARMY.jpg',
+         description:"Lorem ipsum dolor sit amet  dsf", 
+        },
+        {
+         link:'./img/ARMY.jpg',
+         description:"Lorem ipsum dolor sit amet  dsf", 
+        },
+    ]
+
+    TV.forEach(element=>{
+        const body =$(`body`)
+        const movieFavourite =$(`.movieFavourite`)
+        
+         const Favourite=$(` <div class="tv"><img src="${element.link}" alt=""><div class="description">
+         <p>${element.description}</p>
+         <i class="bi bi-heart"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+             <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+           </svg></i>
+        </div>
+        </div>`)
+        $("svg").on(`click`,()=>{ //edit 
+            $(`.top`).show()
+        })
+        Favourite.appendTo(movieFavourite)
+        })
+
+
+
+
+
+
+        //Top
+    let Top=[
+        {
+         link:'./img/ARMY.jpg',
+         description:"Lorem ipsum dolor sit amet", //p
+        },
+        {
+         link:'./img/madMax.jpg',
+         description:"Lorem ipsum dolor sit amet ", 
+        },
+        {
+         link:'./img/friends.jpeg',
+         description:"Lorem ipsum dolor sit amet ", 
+        },
+        {
+         link:'./img/mebeforeyou.jpg',
+         description:"Lorem ipsum dolor sit amet  dsf", 
+        },
+        {
+         link:'./img/Extraction.jpg',
+         description:"Lorem ipsum dolor sit amet  dsf", 
+        },
+        {
+         link:'./img/ARMY.jpg',
+         description:"Lorem ipsum dolor sit amet  dsf", 
+        },
+    ]
+
+    Top.forEach(element=>{
+        const body =$(`body`)
+        const topMovie =$(`.topMovie`)
+        
+         const top=$(` <div class="tv"><img src="${element.link}" alt=""><div class="description">
+         <p>${element.description}</p>
+         <i class="bi bi-heart"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+             <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+           </svg></i>
+        </div>
+        </div>`)
+        $("svg").on(`click`,()=>{ //edit 
+            $(`.top`).show()
+        })
+        top.appendTo(topMovie)
+        })
