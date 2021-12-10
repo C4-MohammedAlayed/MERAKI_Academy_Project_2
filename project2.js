6bacca36b2f36900afb4d7eced48b51091d887b8
MohammedAlayed <alayedmohammed0@gmail.com>
2021-12-10 11:39:46 +0200

    action sort

--- project2.js
+++ project2.js
@@ -20,3 +20,34 @@ $(".showpage").on(`click`,()=>{
     $(".top").hide()
 })
 
+
+// const body =$("body")
+// const main =$( `.main`) //<div class="main"></div>
+// main.appendTo(body)
+// const page =$( `.page`)
+// page.appendTo(main)
+// const navbar=$(`<div class="nav">
+// <div class="logo">
+//     <h1>NETFLEX</h1>
+// </div>
+// <div class="cate">
+//     <ul>
+//         <li><a href="#">Home</a></li>
+//         <li><a href="#">Action</a></li>
+//         <li><a href="#">Drama</a></li>
+//         <li><a href="#">TV</a></li>
+//     </ul>
+// </div>
+// <div class="sort">
+// <button class="showTop">Top</button>
+// <button class="showFavourite">Favourite</button>
+// <button class="showHistory" >History</button>
+
+// </div>
+// </div>`)
+
+// navbar.appendTo(page)
+// مشكلة غدم عمل ازرار buttom
+
+
+
