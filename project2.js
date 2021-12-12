fcce940f1b5680325ff9767911e9c36304d0b8d5
MohammedAlayed <alayedmohammed0@gmail.com>
2021-12-12 10:32:42 +0200

    clean code

--- project2.js
+++ project2.js
@@ -82,7 +82,7 @@ $(`#light`).on(`click`, () => {
   });
   
 });
-//end
+//dark mode
 $(`#dark`).on(`click`, () => {
   $(".movieThree").css({
     "background-color": "black",
@@ -125,35 +125,7 @@ $(`#remove`).on(`click`, () => {
   location.reload();
 });
 
-// const body =$("body")
-// const main =$( `.main`) //<div class="main"></div>
-// main.appendTo(body)
-// const page =$( `.page`)
-// page.appendTo(main)
-// const navbar=$(`<div class="nav">
-// <div class="logo">
-//     <h1>NETFLEX</h1>
-// </div>
-// <div class="cate">
-//     <ul>
-//         <li><a href="#">Home</a></li>
-//         <li><a href="#">Action</a></li>
-//         <li><a href="#">Drama</a></li>
-//         <li><a href="#">TV</a></li>
-//     </ul>
-// </div>
-// <div class="sort">
-// <button class="showTop">Top</button>
-// <button class="showFavourite">Favourite</button>
-// <button class="showHistory" >History</button>
 
-// </div>
-// </div>`)
-
-// navbar.appendTo(page)
-// مشكلة غدم عمل ازرار buttom
-
-//FAVourirte
 
 //..........action......//
 
@@ -161,7 +133,7 @@ let action = [
   {
     id: "1",
     link: "./img/GOT.jpg",
-    description: "action series  war <br> IMDb :9.2/10", //p
+    description: "action series  war <br> IMDb :9.2/10", 
   },
   {
     id: "2",
@@ -369,15 +341,6 @@ Top.forEach((element) => {
 
   top.appendTo(topMovie);
 });
-/*Save*/
-
-//FAVourirte
-// let favourirte=[
-//     {
-//         link: "./img/friends.jpeg",
-//         description: "comedy series family IMDb :8.8/10",
-//       },
-// ]
 
 favourirte.forEach((element, index) => {
   const body = $(`body`);
@@ -392,7 +355,6 @@ favourirte.forEach((element, index) => {
         </div>
         </div>`);
   $("svg").on(`click`, () => {
-    //edit
     Favourite.remove();
     favourirte.splice(index, 1);
     if (favourirte.length > 0) {
@@ -400,43 +362,8 @@ favourirte.forEach((element, index) => {
     } else {
       localStorage.setItem("Myfav", JSON.stringify([]));
     }
-
-    //
-    //
   });
-
   Favourite.appendTo(movieFavourite);
 });
 
-//old
-// function addFav(obj) {
-//   alert("move was added");
-//   const body = $(`body`);
-//   const movieFavourite = $(`.movieFavourite`);
-//   const Favourite =
-//     $(` <div class="tv"><img src="${obj.link}" alt=""><div class="description">
-//                  <p>${obj.description}</p>
-//                  <i class="bi bi-heart"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-//                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-//                    </svg></i>
-//                 </div>
-//                 </div>`);
-//   Favourite.appendTo(movieFavourite);
-// }
 
-// $(`svg`).on("click", () => {
-//     addFav(element);
-//     console.log(index);
-//     element.isPropagationStopped();
-
-// localstorge.setItem(' Favourite', arr)
-
-// localstorge.getItem('Favourite')
-
-//localstorge.setItem('Myfav', JSON.stringfy(arr))
-
-// localstorge.getItem('Myfav')
-
-//JSON.parse(localstorge.getItem('Myfav'))
-
-// save 2
