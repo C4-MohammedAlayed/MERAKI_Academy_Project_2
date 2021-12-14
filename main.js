f1745d014a1d052b70df6eadbdced6167bc1fbfa
MohammedAlayed <alayedmohammed0@gmail.com>
2021-12-14 20:27:13 +0200

    message and user show done

--- main.js
+++ main.js
@@ -61,15 +61,15 @@ $("svg").on(`click`, () => {
 
 $(".signup").on("click",()=>{
   $(".pageLogin").show()
-  massage.hide()
+  
   $(".pageLogin").css({
     display:"flex"
   })
+  massage.hide()
 })
-let account =[{
-  user1 :"ayed",
-  password:"flex"
-}]
+
+let message = $(`<P style="color: red;">wrong password or username</P>`)
+message.appendTo($(`.face`))
 
 console.log();
 let login = $(`#usernameL`)
@@ -79,6 +79,7 @@ let registerP = $(`#passwordR`)
 
 $("#register").on("click", () => {
   $(".login").hide();
+  message.hide()
 });
  
 $("#back").on("click", () => {
@@ -88,15 +89,20 @@ $("#back").on("click", () => {
   $(".login").show();
   
 });
- 
-let massage = $(`<P style="color: red;">wrong password or user name</P>`)
-massage.appendTo($(`.face`))
+ let user =$(`<span></span>`)
+
+
+
+
 $("#enter").on("click", () => {
 if (login.val()=== register.val() && loginP.val()=== registerP.val() ) {
   $(".pageLogin").hide();
+  user.text(`${login.val()}`)
+  user.appendTo($(`#name`))
+  $(`#name`).show()
 
 }else{
-  massage.show()
+  message.show()
   
 }
    
@@ -213,7 +219,7 @@ let heart = `<i id="heart" class="bi bi-heart"><svg  xmlns="http://www.w3.org/20
 <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
 </svg></i>`;
 
-let trash = `<i class="bi bi-trash"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+let trash = `<i id ="trash" class="bi bi-trash"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
 </svg></i>`;
@@ -416,7 +422,7 @@ favourirte.forEach((element, index) => {
 
   const favouriteMovie =
     $(` <div class="tv"><img src="${element.link}" alt=""><div class="description">
-         <p>${element.description}</p>
+         <p>${element.description}${star}</p>
          ${trash}
         </div>
         </div>`);
